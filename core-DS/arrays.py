8951a469de05acc7d599663580e55d25d6e7b804
Suman pokhrel <spokhrel196@gmail.com>
2022-10-01 12:33:13 +0545

    deletion operation implemented

--- core-DS/arrays.py
+++ core-DS/arrays.py
@@ -1,26 +1,29 @@
 import os
+import sys
 import time
 from colorama import Fore
 
 
 # GLOABL VARS
+# DEFAULT VALUE
 user_array = [11, 22 , 33, 44, 55]
 
 def create_Array():
+    os.system("cls")
     global user_array
     print(Fore.YELLOW+"""
     --------------CREATE AN ARRAY------------------
     
     Choose any of the following options
-    1. Enter the length of array
+    1. Enter  elements for your array as Example: 1_2_3_4_5 
     2. Exit
     """)
-    inp = input()
+    inp = int(input())
 
-    if inp == "1":
+    if inp == 1:
         try:
             print(f"Enter  elements for your array as Example: 1_2_3_4_5 ")
-            user_array =list( map(int,input("\x1B[31m "+"").split()))
+            user_array = list( map(int,input("\x1B[31m ").split()))
             print("Array created => ",user_array)
             return user_array
         except Exception as e:
@@ -30,6 +33,7 @@ def create_Array():
 
 
 def insert_element():
+    os.system("cls")
     global user_array
     print(Fore.YELLOW+"""
     --------------INSERTING AN ELEMENT IN ARRAY------------------
@@ -51,6 +55,34 @@ def insert_element():
         elif inp==3:
             elem = int(input("Provide an element to insert : "))
             user_array.append(elem)
+        else:
+            return None
+
+
+def delete_element():
+    os.system("cls")
+    global user_array
+    print(Fore.YELLOW+"""
+    --------------Deleting AN ELEMENT IN ARRAY------------------
+    
+    Choose any of the following options
+    1. Delete at beginning
+    2. Delete at middle
+    3. Delete at last
+    4. Exit
+    """)
+    inp = int(input())
+    if inp<=3 and inp >=1:
+        if inp==1:
+            user_array.pop(0)
+        elif inp==2:
+            index = int(input(f"Provide  index) index range : 0 - {len(user_array)} :  "))
+            if index in range(0,len(user_array)):
+                user_array.pop(index)
+            else:
+                print("Invalid Index")
+        elif inp==3:
+            user_array.pop()
         else:
             return None
 
@@ -89,7 +121,7 @@ if __name__ == "__main__":
         elif user_inp =="2":
             insert_element()
         elif user_inp =="3":
-            pass
+            delete_element()
         elif user_inp =="4":
             pass
         elif user_inp =="5":
