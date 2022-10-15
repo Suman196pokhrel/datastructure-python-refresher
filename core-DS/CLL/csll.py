5903f84306f808531ddbac0b2db8fe0641adca59
Suman Pokhrel <spokhrel196@gmail.com>
2022-10-15 15:23:17 +0545

    print function added to csll

--- core-DS/CLL/csll.py
+++ core-DS/CLL/csll.py
@@ -10,6 +10,22 @@ class Node:
 class CircularLinkedList:
     def __init__(self):
         self.head = None
+
+
+    def __str__(self):
+        if self.head is None:
+            return "Circular Linked List is Empty"
+        else:
+            itr = self.head
+            csll = "self.head ==> "
+            while itr:
+                csll = csll +" ---> " +str(itr.data)
+                itr = itr.next
+                if itr is self.head:
+                    break
+            return csll
+
+
     
     def insert_at_start(self,data):
         if self.head is None:
@@ -19,8 +35,9 @@ class CircularLinkedList:
         else:
             itr = self.head
             node = Node(data)
-            while itr.next:
+            while itr.next is not self.head:
                 itr= itr.next
+
 
             node.next = self.head   
             self.head = node
@@ -35,3 +52,10 @@ class CircularLinkedList:
 
 if __name__=="__main__":
     csll = CircularLinkedList()
+    csll.insert_at_start(55)
+    csll.insert_at_start(44)
+    csll.insert_at_start(33)
+    csll.insert_at_start(22)
+    csll.insert_at_start(11)
+
+    print(csll)
